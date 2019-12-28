064ab37e12a0022cd8fe177cb9122ab64529e21c
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-28 12:34:32 +0100

    Day 7 solution

---
+++ src/day07/index.ts
@@ -0,0 +1,165 @@
+import {
+  test,
+  readInput,
+  arr,
+  com,
+  mul,
+  dis,
+  math,
+  iter,
+  R,
+  graph,
+  log,
+} from "../utils/index"
+
+const getMem = (mem) => {
+  const recur = (x) =>
+    Number.isNaN(Number(x))
+      ? mem[x] !== undefined
+        ? recur(mem[x])
+        : mem[x]
+      : Number(x)
+
+  return recur
+}
+
+const set = (x) => (Number.isNaN(Number(x)) ? x : Number(x))
+
+const compute = (source: string, mem = {}) => {
+  const program = source.split("\n").map((line) => {
+    const chunks = line.split(" ")
+
+    if (chunks.length === 5) {
+      return { op: chunks[1], a: chunks[0], b: chunks[2], c: chunks[4] }
+    }
+
+    if (chunks.length === 4) {
+      return { op: chunks[0], a: chunks[1], c: chunks[3] }
+    }
+
+    return { op: "LINK", a: chunks[0], c: chunks[2] }
+  })
+
+  const MODULUS = 65536
+  const get = getMem(mem)
+
+  const done = new Set()
+
+  while (done.size !== program.length) {
+    program.forEach(({ op, a, b, c }, index) => {
+      switch (op) {
+        case "LINK": {
+          mem[c] = set(a)
+          done.add(index)
+          break
+        }
+        case "AND": {
+          if (
+            !done.has(index) &&
+            get(a) !== undefined &&
+            get(b) !== undefined
+          ) {
+            mem[c] = math.mod(get(a) & get(b), MODULUS)
+            done.add(index)
+          }
+          break
+        }
+        case "OR": {
+          if (
+            !done.has(index) &&
+            get(a) !== undefined &&
+            get(b) !== undefined
+          ) {
+            mem[c] = math.mod(get(a) | get(b), MODULUS)
+            done.add(index)
+          }
+          break
+        }
+        case "LSHIFT": {
+          if (
+            !done.has(index) &&
+            get(a) !== undefined &&
+            get(b) !== undefined
+          ) {
+            mem[c] = math.mod(get(a) << get(b), MODULUS)
+            done.add(index)
+          }
+          break
+        }
+        case "RSHIFT": {
+          if (
+            !done.has(index) &&
+            get(a) !== undefined &&
+            get(b) !== undefined
+          ) {
+            mem[c] = math.mod(get(a) >> get(b), MODULUS)
+            done.add(index)
+          }
+          break
+        }
+        case "NOT": {
+          if (!done.has(index) && get(a) !== undefined) {
+            mem[c] = math.mod(~get(a), MODULUS)
+            done.add(index)
+          }
+          break
+        }
+      }
+    })
+  }
+
+  return mem
+}
+
+const goA = (source: string) => {
+  const mem = compute(source)
+
+  return getMem(mem)("a")
+}
+
+const goB = (source: string) => {
+  const mem = compute(source)
+  const alteredSource = source.replace("1674", String(getMem(mem)("a")))
+  const mem2 = compute(alteredSource)
+
+  return getMem(mem2)("a")
+}
+
+/* Tests */
+
+test(
+  compute(
+    `
+123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i
+  `.trim(),
+  ),
+  {
+    d: 72,
+    e: 507,
+    f: 492,
+    g: 114,
+    h: 65412,
+    i: 65079,
+    x: 123,
+    y: 456,
+  },
+)
+
+/* Results */
+
+const input = readInput()
+
+console.time("Time")
+const resultA = goA(input)
+const resultB = goB(input)
+console.timeEnd("Time")
+
+console.log("Solution to part 1:", resultA) // -> 46065
+console.log("Solution to part 2:", resultB) // -> 14134
